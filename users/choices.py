cb7ac15d405a89a1e5c568acd8dad9ac8250f646
Hamed <hami.fthi@gmail.com>
2020-02-15 17:46:11 +0330

    athlete and coach model is added

---
+++ users/choices.py
@@ -0,0 +1,21 @@
+Sex_Choices = (
+    ('F', 'Female'),
+    ('M', 'Male'),
+)
+Sport_Field = (
+    ('A', 'Aerobic'),
+    ('F', 'Fitness'),
+    ('T', 'TRX'),
+    ('B', 'Bodybuilding'),
+    ('P', 'Physic'),
+    ('C', 'Crossfit')
+)
+Day_Choices = (
+    ('1', 'Saturday'),
+    ('2', 'Sunday'),
+    ('3', 'Monday'),
+    ('4', 'Tuesday'),
+    ('5', 'Wednesday'),
+    ('6', 'Thursday'),
+    ('7', 'Friday')
+)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from datetime import time
+from multiselectfield import MultiSelectField
+from .choices import *
 
 # Create your models here.
+# The person class which contains credentials of a person whether he or she is athelte or Coach
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=1, choices=Sex_Choices)
+    sport_field = models.CharField(max_length=1, choices=Sport_Field)
+    days_of_week = MultiSelectField(choices=Day_Choices)
+
+class Coach(Person):
+    salary = models.BigIntegerField()
+    start_time = models.TimeField(null=False, blank=False)
+    end_time = models.TimeField(null=False, blank=False)
+    
+    def __str__(self):
+        return f'{self.name }_{self.last_name}'
+
+class Athelte(Person):
+    last_payment = models.DateField()
+    trainer = models.ForeignKey(Coach, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name }_{self.last_name}'
