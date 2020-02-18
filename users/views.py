4955b7359d46abe04408169e762e66b878f7fab7
Hamed <hami.fthi@gmail.com>
2020-02-18 18:45:13 +0330

    cleaning views and deleting duplicated code

--- users/views.py
+++ users/views.py
@@ -11,19 +11,13 @@ import os, binascii
 @method_decorator(csrf_exempt, name='dispatch')
 class CreateAthlete(View):
     def post(self, request, *args, **kargs):
-        name = request.POST['name']
-        last_name = request.POST['last_name']
-        age =request.POST['age']
-        sex = request.POST['sex']
-        sport_field = request.POST['sport_field']
-        days_of_week = request.POST['days_of_week']
-        last_payment = request.POST['last_payment']
-        trainer_name = request.POST['trainer']
-        trainer = Coach.objects.filter(name = trainer_name)[0]
+        trainer = Coach.objects.filter(name = request.POST['trainer'])[0]
         token = binascii.b2a_hex(os.urandom(64))
-        athlete, _ = Athlete.objects.get_or_create(name=name, last_name=last_name, age=age, 
-        sex=sex, sport_field=sport_field, days_of_week=days_of_week,
-        last_payment=last_payment, trainer=trainer)
+        athlete, _ = Athlete.objects.get_or_create(name=request.POST['name'],
+        last_name=request.POST['last_name'], age=request.POST['age'],
+        sex=request.POST['sex'], sport_field=request.POST['sport_field'],
+        days_of_week=request.POST['days_of_week'],
+        last_payment=request.POST['last_payment'], trainer=trainer)
         Token.objects.create(user=athlete, token=token)
         return JsonResponse({
             'status': 'ok'
@@ -32,19 +26,13 @@ class CreateAthlete(View):
 @method_decorator(csrf_exempt, name='dispatch')
 class CreateCoach(View):
     def post(self, request, *args, **kargs):
-        name = request.POST['name']
-        last_name = request.POST['last_name']
-        age =request.POST['age']
-        sex = request.POST['sex']
-        sport_field = request.POST['sport_field']
-        days_of_week = list(request.POST['days_of_week'])
-        salary = request.POST['salary']
-        start_time = request.POST['start_time']
-        end_time = request.POST['end_time']
         token = binascii.b2a_hex(os.urandom(64))
-        coach, _ = Coach.objects.get_or_create(name=name, last_name=last_name, age=age, 
-        sex=sex, sport_field=sport_field, days_of_week=days_of_week,
-        salary=salary, start_time=start_time, end_time=end_time)
+        coach, _ = Coach.objects.get_or_create(name=request.POST['name'],
+        last_name=request.POST['last_name'], age=request.POST['age'],
+        sex=request.POST['sex'], sport_field=request.POST['sport_field'],
+        days_of_week=request.POST['days_of_week'],
+        salary=request.POST['salary'], start_time=request.POST['start_time'],
+        end_time=request.POST['end_time'])
         Token.objects.create(user=coach, token=token)
         return JsonResponse({
             'status': 'ok'
