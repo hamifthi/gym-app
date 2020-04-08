435dd741a6007e1ee0771ff69eb2812f9929fd52
Hamed <hami.fthi@gmail.com>
2020-04-08 17:21:04 +0430

    design the page of submit income and expense

--- finance/forms.py
+++ finance/forms.py
@@ -2,14 +2,31 @@ from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 from django import forms
 
+from .widgets import BootstrapDateTimePickerInput
 from .models import Income, Expense
 
 class IncomeSubmitForm(ModelForm):
+
     class Meta:
         model = Income
-        exclude = ['code']
+        exclude = ['user', 'code']
+        help_texts = {'details': ('Please describe how do you earn this income'),
+                                'amount': ('How much do you make?'),}
+        error_messages = {'details': {'invalid': 'You must explain how you earn this income'},
+                                          'amount': {'invalid': 'How much this is important. don\' forget that'}}
+        widgets =  {
+            'date': BootstrapDateTimePickerInput(), # datepicker pop up
+        }
+
 
 class ExpenseSubmitForm(ModelForm):
     class Meta:
         model = Expense
-        exclude = ['code']
+        exclude = ['user', 'code']
+        help_texts = {'details': ('Please describe how do you spend this money'),
+                                'amount': ('How much do you spend?'),}
+        error_messages = {'details': {'invalid': 'You must explain how you spend this money'},
+                                          'amount': {'invalid': 'How much this is important. don\' forget that'}}
+        widgets =  {
+            'date': BootstrapDateTimePickerInput(), # datepicker pop up
+        }
